f5657357278456b26c70959d0964a8cf25a69acc
VishnuVaibhavRD <vishnuvaibhavreddy@gmail.com>
2022-11-27 13:50:34 +0530

    deploy

---
+++ pages/resume.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from "next/image";
+import pro4 from "../public/pro4.png";
+
+const resume = () => {
+  return (
+    <main className='px-10 md:px-20 lg:px-40 bg-gray-600'>
+    <section className="min-h-screen">
+    <div>
+      <h1 className="font-burtons text-4xl flex justify-center my-auto items-center min-h-screen">work in process my friend...</h1>
+    </div>
+    </section>
+    </main>
+  );
+}
+
+export default resume
